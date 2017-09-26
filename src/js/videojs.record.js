99fcd4437a63de0ac74b0823d30909dc6e572bbf
Thijs Triemstra <info@collab.nl>
2017-09-26 06:46:50 +0200

    add getusermedia module

--- src/js/videojs.record.js
+++ src/js/videojs.record.js
@@ -15,6 +15,7 @@ import * as base from './engine/record-base';
 import RecordRTCEngine from './engine/record-rtc';
 import pluginDefaultOptions from './defaults';
 import formatTime from './utils/format-time';
+import loadGetUserMedia from './utils/getusermedia';
 import { detectBrowser, isChrome } from './utils/detect-browser';
 
 import videojs from 'video.js';
@@ -58,39 +59,8 @@ class Recorder extends Plugin {
         // (re)set recorder state
         this.resetState();
 
-        // cross-browser getUserMedia
-        var promisifiedOldGUM = function(constraints, successCallback, errorCallback) {
-            // get ahold of getUserMedia, if present
-            var getUserMedia = (navigator.getUserMedia ||
-                navigator.webkitGetUserMedia ||
-                navigator.mozGetUserMedia ||
-                navigator.msGetUserMedia);
-            // Some browsers just don't implement it - return a rejected
-            // promise with an error to keep a consistent interface
-            if (!getUserMedia) {
-                return Promise.reject(
-                    new Error('getUserMedia is not implemented in this browser')
-                );
-            }
-            // otherwise, wrap the call to the old navigator.getUserMedia with
-            // a Promise
-            return new Promise(function(successCallback, errorCallback) {
-                getUserMedia.call(navigator, constraints, successCallback,
-                    errorCallback);
-            });
-        };
-        // Older browsers might not implement mediaDevices at all, so we set an
-        // empty object first
-        if (navigator.mediaDevices === undefined) {
-            navigator.mediaDevices = {};
-        }
-        // Some browsers partially implement mediaDevices. We can't just assign
-        // an object with getUserMedia as it would overwrite existing
-        // properties. Here, we will just add the getUserMedia property if it's
-        // missing.
-        if (navigator.mediaDevices.getUserMedia === undefined) {
-            navigator.mediaDevices.getUserMedia = promisifiedOldGUM;
-        }
+        // setup cross-browser getUserMedia
+        loadGetUserMedia();
 
         // wait until player ui is ready
         this.player.one('ready', this.setupUI.bind(this));
