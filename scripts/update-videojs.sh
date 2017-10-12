93a95c949ff1bd5a8691904b8485a87509524b13
Thijs Triemstra <info@collab.nl>
2017-10-13 01:14:20 +0200

    add video.js example update script

---
+++ scripts/update-videojs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export OLD_VERSION=6.3.2
+export NEW_VERSION=6.2.8
+
+# check if language files exist
+wget --delete-after https://vjs.zencdn.net/$NEW_VERSION/lang/nl.js
+
+# update videojs in examples and documentation
+find examples/ -type f -name '*.html' -exec sed -i "s/$OLD_VERSION/$NEW_VERSION/" {} \;
+
+#git commit -am "bump videojs to $NEW_VERSION in examples"
