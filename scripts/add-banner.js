0360b850bf3f0ecae91598e288aeedf0a92eafb0
Thijs Triemstra <info@collab.nl>
2017-11-27 23:59:10 +0100

    use glob

--- scripts/add-banner.js
+++ scripts/add-banner.js
@@ -7,6 +7,7 @@
 
 var fs = require('fs');
 var path = require('path');
+var glob = require('glob')
 var moment = require('moment');
 var banner = require('add-banner');
 
@@ -16,14 +17,15 @@ var infile, fpath, result;
 var pjson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
 // check command-line arguments
-if (process.argv.length < 3) {
+if (process.argv.length != 3) {
     console.error("Error: no input file(s) specified");
     process.exit();
 }
 
 // process multiple files
-process.argv.forEach(function (fpath, index, array) {
-    if (index > 1) {
+glob(process.argv[2], function (er, files) {
+    files.forEach(function(fpath) {
+
         infile = fs.readFileSync(fpath, 'utf8');
         if (fpath.endsWith(CSS)) {
             // CSS
@@ -32,7 +34,7 @@ process.argv.forEach(function (fpath, index, array) {
                 pkg: pjson
             });
             fs.writeFile(fpath, result);
-
+    
         } else if (fpath.endsWith(JS)) {
             // javascript
             result = banner(infile, {
@@ -43,7 +45,9 @@ process.argv.forEach(function (fpath, index, array) {
             fs.writeFile(fpath, result);
         }
         console.info('Added banner to ' + fpath);
-    }
+    });
 });
+
+
 
 
