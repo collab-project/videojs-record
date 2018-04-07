f30d71ccfcaca0f5954ba78d91e1189ee2d2e7d6
Thijs Triemstra <info@collab.nl>
2018-04-08 01:05:53 +0200

    update video.js to 6.8.0 in examples

--- scripts/update-videojs.sh
+++ scripts/update-videojs.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-export OLD_VERSION=6.6.0
-export NEW_VERSION=6.6.3
+export OLD_VERSION=6.7.3
+export NEW_VERSION=6.8.0
+
+echo
+echo "Update video.js to v"$NEW_VERSION
+echo
 
 # check if language files exist
-wget --delete-after https://vjs.zencdn.net/$NEW_VERSION/lang/nl.js
+#wget --delete-after https://vjs.zencdn.net/$NEW_VERSION/lang/nl.js
 
 # update videojs in examples and documentation
-find examples/ -type f -name '*.html' -exec sed -i "s/$OLD_VERSION/$NEW_VERSION/" {} \;
+if [ "$(uname)" == "Darwin" ]; then
+    find . -type f -name '*.html' -exec sed -i '' s/$OLD_VERSION/$NEW_VERSION/ {} +
+else
+    find examples/ -type f -name '*.html' -exec sed -i "s/$OLD_VERSION/$NEW_VERSION/" {} \;
+fi
 
 #git commit -am "bump videojs to $NEW_VERSION in examples"
