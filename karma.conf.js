b0ccce3b205bbd3d694089fbb5787a98d0cb847c
Thijs Triemstra <info@collab.nl>
2018-04-29 01:41:56 +0200

    use custom chrome user data dir

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,9 @@ var chromeFlags = [
     '--use-fake-ui-for-media-stream',
     '--use-file-for-fake-audio-capture=test/support/Front_Center.wav',
     '--use-file-for-fake-video-capture=test/support/bus_qcif_7.5fps.y4m',
-    '--autoplay-policy=no-user-gesture-required'
+    '--autoplay-policy=no-user-gesture-required',
+    '--user-data-dir=chrome-user-data',
+    '--no-default-browser-check'
 ];
 
 module.exports = function(config) {
