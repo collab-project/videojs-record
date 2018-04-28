a7acc72a54a140ad3596b26f2f9323faee17fb48
Thijs Triemstra <info@collab.nl>
2018-04-29 01:54:44 +0200

    enable more chrome flags

--- karma.conf.js
+++ karma.conf.js
@@ -10,15 +10,20 @@ var webpackConfig = require('./build-config/webpack.prod.main.js');
 
 var chromeFlags = [
     '--no-sandbox',
+    '--no-first-run',
+    '--noerrdialogs',
+    '--no-default-browser-check',
     '--use-fake-device-for-media-stream',
     '--use-fake-ui-for-media-stream',
     '--use-file-for-fake-audio-capture=test/support/Front_Center.wav',
     '--use-file-for-fake-video-capture=test/support/bus_qcif_7.5fps.y4m',
     '--autoplay-policy=no-user-gesture-required',
     '--user-data-dir=chrome-user-data',
-    '--no-default-browser-check',
     '--disable-translate',
-    '--disable-extensions'
+    '--disable-extensions',
+    '--disable-infobars',
+    '--ignore-certificate-errors',
+    '--allow-insecure-localhost'
 ];
 
 module.exports = function(config) {
