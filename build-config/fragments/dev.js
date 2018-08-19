53be457a8fb30caba9a2137a97c7adccc144ddd3
Thijs Triemstra <info@collab.nl>
2018-08-20 00:55:24 +0200

    add start of websocket upload example

--- build-config/fragments/dev.js
+++ build-config/fragments/dev.js
@@ -3,14 +3,38 @@
  * @since 2.2.0
  */
 
-const path = require('path');
 const fs = require('fs');
+const http = require('http');
+const path = require('path');
 const util = require('util');
-const formidable = require('formidable');
+const uuid = require('uuid');
+const sockjs = require('sockjs');
 const colors = require('colors/safe');
+const formidable = require('formidable');
+const node_static = require('node-static');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const contentBase = path.resolve(__dirname, '..', '..');
+
+function writeToDisk(dataURL, fileName) {
+    var fileExtension = fileName.split('.').pop(),
+        fileRootNameWithBase = './uploads/' + fileName,
+        filePath = fileRootNameWithBase,
+        fileID = 2,
+        fileBuffer;
+
+    console.log(colors.yellow('filename:', filePath));
+
+    // @todo return the new filename to client
+    while (fs.existsSync(filePath)) {
+        filePath = fileRootNameWithBase + '(' + fileID + ').' + fileExtension;
+        fileID += 1;
+    }
+
+    dataURL = dataURL.split(',').pop();
+    fileBuffer = new Buffer(dataURL, 'base64');
+    fs.writeFileSync(filePath, fileBuffer);
+}
 
 module.exports = {
     mode: 'development',
@@ -21,7 +45,9 @@ module.exports = {
         watchContentBase: true,
         // webpack-dev-server middleware
         before(app) {
+            // =============================================
             // use proper mime-type for wasm files
+            // =============================================
             app.get('*.wasm', function(req, res, next) {
                 var options = {
                     root: contentBase,
@@ -36,7 +62,9 @@ module.exports = {
                     }
                 });
             });
-            // file upload handler for examples
+            // =============================================
+            // file upload handler for simple upload example
+            // =============================================
             app.post('/upload', function(req, res) {
                 // save uploaded file
                 var form = new formidable.IncomingForm();
@@ -64,6 +92,49 @@ module.exports = {
 
                 return;
             });
+            console.log('');
+            console.log(colors.green(' [examples] /upload handler ready'));
+            console.log('');
+
+            // ===================================================
+            // streaming file upload handler for websocket example
+            // ===================================================
+            var sockjs_echo = sockjs.createServer();
+            sockjs_echo.on('connection', function(conn) {
+                conn.on('data', function(data) {
+
+                    console.log('saving uploaded file...');
+
+                    var fileName = uuid.v4() + '.ogg';
+                    writeToDisk(data, fileName);
+
+                    console.log(colors.green('saved file.'));
+                    console.log('');
+                });
+            });
+
+            // static files server
+            var static_directory = new node_static.Server(__dirname);
+            var server = http.createServer();
+            server.addListener('request', function(req, res) {
+                static_directory.serve(req, res);
+            });
+            server.addListener('upgrade', function(req,res){
+                res.end();
+            });
+            sockjs_echo.installHandlers(server, {prefix: '/echo'});
+
+            console.log('');
+            console.log(colors.green(' [examples] Websocket server listening on ') +
+                        colors.yellow('0.0.0.0:9999'));
+            server.listen(9999, '0.0.0.0');
+            console.log('');
+        },
+        proxy: {
+            '/echo': {
+                target: 'http://0.0.0.0:9999',
+                ws: true
+            }
         }
     },
     plugins: [
