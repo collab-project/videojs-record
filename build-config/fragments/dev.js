f0ead1e3e09dd47a6bb7ba4ae2b2bdff65a16567
Thijs Triemstra <info@collab.nl>
2018-08-20 00:55:24 +0200

    add start of websocket upload example

--- build-config/fragments/dev.js
+++ build-config/fragments/dev.js
@@ -3,12 +3,37 @@
  * @since 2.2.0
  */
 
-const path = require('path');
 const fs = require('fs');
+const http = require('http');
+const path = require('path');
 const util = require('util');
-const formidable = require('formidable');
+const uuid = require('uuid');
+const sockjs = require('sockjs');
 const colors = require('colors/safe');
+const formidable = require('formidable');
+const node_static = require('node-static');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+function writeToDisk(dataURL, fileName) {
+    var fileExtension = fileName.split('.').pop(),
+        fileRootNameWithBase = './uploads/' + fileName,
+        filePath = fileRootNameWithBase,
+        fileID = 2,
+        fileBuffer;
+
+    console.log(colors.yellow('filename:', filePath));
+
+    // @todo return the new filename to client
+    while (fs.existsSync(filePath)) {
+        filePath = fileRootNameWithBase + '(' + fileID + ').' + fileExtension;
+        fileID += 1;
+    }
+
+    dataURL = dataURL.split(',').pop();
+    fileBuffer = new Buffer(dataURL, 'base64');
+    fs.writeFileSync(filePath, fileBuffer);
+}
 
 module.exports = {
     mode: 'development',
@@ -18,7 +43,9 @@ module.exports = {
         publicPath: '/',
         watchContentBase: true,
         before(app){
-            // file upload handler for examples
+            // =============================================
+            // file upload handler for simple upload example
+            // =============================================
             app.post('/upload', function(req, res) {
                 // save uploaded file
                 var form = new formidable.IncomingForm();
@@ -46,6 +73,49 @@ module.exports = {
 
                 return;
             });
+            console.log('');
+            console.log(colors.green(' [examples] /upload handler ready'));
+            console.log('');
+
+            // ===================================================
+            // streaming file upload handler for websocket example
+            // ===================================================
+            var sockjs_echo = sockjs.createServer();
+            sockjs_echo.on('connection', function(conn) {
+                conn.on('data', function(data) {
+
+                    console.log('saving uploaded file...');
+
+                    var fileName = uuid.v4() + '.ogg';
+                    writeToDisk(data, fileName);
+
+                    console.log(colors.green('saved file.'));
+                    console.log('');
+                });
+            });
+
+            // static files server
+            var static_directory = new node_static.Server(__dirname);
+            var server = http.createServer();
+            server.addListener('request', function(req, res) {
+                static_directory.serve(req, res);
+            });
+            server.addListener('upgrade', function(req,res){
+                res.end();
+            });
+            sockjs_echo.installHandlers(server, {prefix:'/echo'});
+
+            console.log('');
+            console.log(colors.green(' [examples] Websocket server listening on ') +
+                        colors.yellow('0.0.0.0:9999'));
+            server.listen(9999, '0.0.0.0');
+            console.log('');
+        },
+        proxy: {
+            '/echo': {
+                target: 'http://0.0.0.0:9999',
+                ws: true
+            }
         }
     },
     plugins: [
