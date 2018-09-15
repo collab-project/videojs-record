fe85af745444dd7e094aff6c3694364e50e61669
Thijs Triemstra <info@collab.nl>
2018-09-16 01:11:11 +0200

    switch to terser-webpack-plugin

--- build-config/fragments/prod.js
+++ build-config/fragments/prod.js
@@ -3,7 +3,7 @@
  * @since 2.2.0
 */
 
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
@@ -12,13 +12,8 @@ module.exports = {
     devtool: 'source-map',
     optimization: {
         minimizer: [
-            new UglifyJSPlugin({
-                sourceMap: true,
-                uglifyOptions: {
-                    compress: {
-                        inline: false
-                    }
-                }
+            new TerserPlugin({
+                sourceMap: true
             }),
             new OptimizeCSSAssetsPlugin({})
         ]
