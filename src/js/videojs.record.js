86b5f93573e98fac9b6240c6f4b58c3864e829a6
Thijs Triemstra <info@collab.nl>
2018-09-29 23:18:35 +0200

    doc update

--- src/js/videojs.record.js
+++ src/js/videojs.record.js
@@ -243,6 +243,7 @@ class Record extends Plugin {
         // hide play control
         this.player.controlBar.playToggle.hide();
 
+        // trigger early warning if screen-only is not supported
         if (this.getRecordType() === SCREEN_ONLY &&
             'getDisplayMedia' in navigator === false) {
             // screen capture not supported in this browser
@@ -379,10 +380,11 @@ class Record extends Plugin {
             case SCREEN_ONLY:
                 // setup screen
                 this.mediaType = {
-                    // animated GIF
+                    // screen capture
                     audio: false,
                     video: false,
-                    screen: true
+                    screen: true,
+                    gif: false
                 };
                 navigator.getDisplayMedia({
                     video: true
