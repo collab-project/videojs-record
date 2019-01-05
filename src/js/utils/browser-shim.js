45a4af6b69275a6fae560763638412847fecdcef
Thijs Triemstra <info@collab.nl>
2019-01-05 23:49:33 +0100

    improve setSrcObject

--- src/js/utils/browser-shim.js
+++ src/js/utils/browser-shim.js
@@ -3,20 +3,13 @@
  * @since 2.0.0
  */
 
-const setSrcObject = function (stream, element, ignoreCreateObjectURL) {
-    if ('createObjectURL' in URL && !ignoreCreateObjectURL) {
-        try {
-            element.src = URL.createObjectURL(stream);
-        } catch (e) {
-            setSrcObject(stream, element, true);
-            return;
-        }
-    } else if ('srcObject' in element) {
+const setSrcObject = function (stream, element) {
+    if ('srcObject' in element) {
         element.srcObject = stream;
     } else if ('mozSrcObject' in element) {
         element.mozSrcObject = stream;
     } else {
-        throw new Error('createObjectURL/srcObject both are not supported.');
+        element.srcObject = stream;
     }
 };
 
