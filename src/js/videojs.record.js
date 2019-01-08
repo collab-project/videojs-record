2df804cbe84a5547dc58061e5e6fa4622d330331
Thijs Triemstra <info@collab.nl>
2019-01-08 02:34:56 +0100

    add injectMetadata

--- src/js/videojs.record.js
+++ src/js/videojs.record.js
@@ -5,6 +5,8 @@
  * MIT license: https://github.com/collab-project/videojs-record/blob/master/LICENSE
  */
 
+import {Decoder, Encoder, tools, Reader} from 'ts-ebml';
+
 import AnimationDisplay from './controls/animation-display';
 import RecordCanvas from './controls/record-canvas';
 import DeviceButton from './controls/device-button';
@@ -1218,6 +1220,39 @@ class Record extends Plugin {
         this._processing = false;
         this._deviceActive = false;
         this.devices = [];
+    }
+
+    readAsArrayBuffer(blob) {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsArrayBuffer(blob);
+            reader.onloadend = () => { resolve(reader.result); };
+            reader.onerror = (ev) => { reject(ev.error); };
+        });
+    }
+
+    injectMetadata() {
+        const decoder = new Decoder();
+        const reader = new Reader();
+
+        reader.logging = false;
+        reader.drop_default_duration = false;
+
+        this.readAsArrayBuffer(this.player.recordedData).then((buffer) => {
+            const elms = decoder.decode(buffer);
+            elms.forEach((elm) => { reader.read(elm); });
+            reader.stop();
+
+            let refinedMetadataBuf = tools.makeMetadataSeekable(
+                reader.metadatas, reader.duration, reader.cues);
+            let body = buffer.slice(reader.metadataSize);
+            let result = new Blob([refinedMetadataBuf, body],
+                {type: this.player.recordedData.type});
+
+            // console.log('ready', result);
+
+            this.player.recordedData = result;
+        });
     }
 
     /**
