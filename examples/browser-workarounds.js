0405379b71cfe2fde338e7441739c5527690f53b
Thijs Triemstra <info@collab.nl>
2019-01-13 14:56:49 +0100

    ignore workaround for Safari with MediaRecorder API

--- examples/browser-workarounds.js
+++ examples/browser-workarounds.js
@@ -5,13 +5,19 @@ var isEdge = /Edge/.test(navigator.userAgent);
 
 function applyAudioWorkaround() {
     if (isSafari || isEdge) {
-        console.log('applied audio workarounds for this browser');
+
+        if (isSafari && window.MediaRecorder !== undefined) {
+            // this version of Safari has MediaRecorder
+            return;
+        }
 
         // see https://github.com/collab-project/videojs-record/issues/295
         options.plugins.record.audioRecorderType = StereoAudioRecorder;
         options.plugins.record.audioSampleRate = 44100;
         options.plugins.record.audioBufferSize = 4096;
         options.plugins.record.audioChannels = 2;
+
+        console.log('applied audio workarounds for this browser');
     }
 }
 
