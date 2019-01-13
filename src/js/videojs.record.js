fe3370d180cf86a2504f167395a567ad14bd033f
Thijs Triemstra <info@collab.nl>
2019-01-13 22:51:05 +0100

    add change video input example (#322)

--- src/js/videojs.record.js
+++ src/js/videojs.record.js
@@ -1464,7 +1464,7 @@ class Record extends Plugin {
             return;
         }
 
-        // List cameras and microphones.
+        // list video and audio devices
         navigator.mediaDevices.enumerateDevices(this).then((devices) => {
             this.devices = [];
             devices.forEach((device) => {
