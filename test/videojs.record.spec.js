cb232735dbf891f56cff28d2a41bc1ba133f2321
Thijs Triemstra <info@collab.nl>
2019-02-28 04:25:59 +0100

    only listen once!

--- test/videojs.record.spec.js
+++ test/videojs.record.spec.js
@@ -490,7 +490,7 @@ describe('Record', () => {
             }, 2000);
         });
 
-        player.on(Event.TIMESTAMP, () => {
+        player.one(Event.TIMESTAMP, () => {
             total += 1;
 
             expect(player.currentTimestamp).toBeDefined();
