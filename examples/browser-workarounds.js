b0e8cd6e584c025a4e3b1f9c326bd1ae9856fd05
Thijs Triemstra <info@collab.nl>
2019-03-04 13:15:41 +0100

    remove safari audio-only browser workaround

--- examples/browser-workarounds.js
+++ examples/browser-workarounds.js
@@ -33,23 +33,3 @@ function applyScreenWorkaround() {
         adapter.browserShim.shimGetDisplayMedia(window, 'screen');
     }
 }
-
-function addStartButton() {
-    var btn = document.createElement('BUTTON');
-    var t = document.createTextNode('Show player');
-    btn.onclick = createPlayer;
-    btn.appendChild(t);
-    document.body.appendChild(btn);
-}
-
-function updateContext(opts) {
-    // Safari 11 or newer automatically suspends new AudioContext's that aren't
-    // created in response to a user-gesture, like a click or tap, so create one
-    // here (inc. the script processor)
-    var AudioContext = window.AudioContext || window.webkitAudioContext;
-    var context = new AudioContext();
-    var processor = context.createScriptProcessor(1024, 1, 1);
-
-    opts.plugins.wavesurfer.audioContext = context;
-    opts.plugins.wavesurfer.audioScriptProcessor = processor;
-}
