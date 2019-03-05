edd63757cfc6a66f99a2900495566ddfa5d51621
Thijs Triemstra <info@collab.nl>
2019-03-06 00:17:58 +0100

    update to v3.5.1

--- dist/plugins/videojs.record.ts-ebml.js
+++ dist/plugins/videojs.record.ts-ebml.js
@@ -1,6 +1,6 @@
 /*!
  * ts-ebml plugin for videojs-record
- * @version 3.5.0
+ * @version 3.5.1
  * @see https://github.com/collab-project/videojs-record
  * @copyright 2014-2019 Collab
  * @license MIT
