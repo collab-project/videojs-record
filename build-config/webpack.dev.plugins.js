a623a1bf147e0f41935a4cce33d17607407ebe52
Thijs Triemstra <info@collab.nl>
2020-07-14 21:35:57 +0200

    update dev dependencies

--- build-config/webpack.dev.plugins.js
+++ build-config/webpack.dev.plugins.js
@@ -1,7 +1,7 @@
 /**
  * @since 2.2.0
  */
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const path = require('path');
 
 const common = require('./fragments/common');
