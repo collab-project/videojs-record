e1c864d0eb39510846a56b0f8f4585a5a7a22217
Thijs Triemstra <info@collab.nl>
2020-10-15 17:36:37 +0200

    remove terser sourceMap option

--- build-config/fragments/prod.js
+++ build-config/fragments/prod.js
@@ -13,7 +13,6 @@ module.exports = {
         minimize: true,
         minimizer: [
             new TerserPlugin({
-                sourceMap: false,
                 parallel: true,
                 terserOptions: {
                     output: {
