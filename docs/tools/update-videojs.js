df511d7f95cde4b7f51c5b646edfeb3f7aa8e414
Thijs Triemstra <info@collab.nl>
2021-04-14 01:12:07 +0200

    examples: bump video.js version to 7.11.8 in demo files

--- docs/tools/update-videojs.js
+++ docs/tools/update-videojs.js
@@ -5,8 +5,8 @@
 const path = require('path');
 const replace = require('replace-in-file');
 
-const OLD_VERSION = "7.10.2";
-const NEW_VERSION = "7.11.4";
+const OLD_VERSION = "video.js@7.11.4";
+const NEW_VERSION = "video.js@7.11.8";
 
 const options = {
   files: path.resolve(__dirname, '..', 'demo') + '/**/*.html',
@@ -16,12 +16,21 @@ const options = {
 };
 
 console.log();
-console.log(`Updating video.js from ${OLD_VERSION} to ${NEW_VERSION} in ${options.files}`);
+console.log(`Updating from ${OLD_VERSION} to ${NEW_VERSION} in ${path.relative('.', options.files)}`);
 console.log();
 
 replace(options)
 .then(results => {
-    console.log('Replacement results:', results);
+    let changes = false;
+    results.forEach(item => {
+        if (item.hasChanged) {
+            console.log("Updated " + path.relative('.', item.file));
+            changes = true;
+        }
+    });
+    if (!changes) {
+        console.log("No files updated.");
+    }
 })
 .catch(error => {
     console.error('Error occurred:', error);
