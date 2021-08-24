58a3134dfb094b69bd8b0793b5e19265f8c60893
Denis Mosolov <denismosolov@gmail.com>
2021-08-24 18:53:27 +0300

    Respect linter

--- src/js/controls/record-toggle.js
+++ src/js/controls/record-toggle.js
@@ -10,6 +10,8 @@ import Event from '../event';
 const Button = videojs.getComponent('Button');
 const Component = videojs.getComponent('Component');
 
+const COUNTDOWN_INTERVAL = 1000;
+
 /**
  * Button to toggle between start and stop recording.
  *
@@ -72,19 +74,22 @@ class RecordToggle extends Button {
     handleClick(event) {
         let recorder = this.player_.record();
         if (!recorder.isRecording()) {
+            let countdown = 3;
+            let down = () => {
+                if (countdown <= 0) {
+                    this.player_.countdownOverlay.hide();
+                    recorder.start();
+                } else {
+                    countdown--;
+                    this.player_.countdownOverlay.setCountdownValue(countdown);
+                    setTimeout(down, COUNTDOWN_INTERVAL);
+                }
+            };
+
             this.player_.countdownOverlay.show();
-            this.player_.countdownOverlay.setCountdownValue(3);
-            setTimeout(() => {
-                this.player_.countdownOverlay.setCountdownValue(2);
-                setTimeout(() => {
-                    this.player_.countdownOverlay.setCountdownValue(1);
-                    setTimeout(() => {
-                        this.player_.countdownOverlay.setCountdownValue(0);
-                        this.player_.countdownOverlay.hide();
-                        recorder.start();
-                    }, 1000);
-                }, 1000);
-            }, 1000);
+            this.player_.countdownOverlay.setCountdownValue(countdown);
+
+            down();
         } else {
             recorder.stop();
         }
