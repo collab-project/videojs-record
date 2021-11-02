ad1e72ee463fcb529e9004cbe1e41534de89bfef
Michael Heydon <michael.heydon@alaress.com.au>
2021-11-03 10:12:59 +1100

    Extend conditional within setVideoInput() to allow switchin video device in image_only mode

--- src/js/videojs.record.js
+++ src/js/videojs.record.js
@@ -1827,6 +1827,9 @@ class Record extends Plugin {
             this.recordVideo = {
                 deviceId: {exact: deviceId}
             };
+        } else if (this.recordImage === Object(this.recordImage)) {
+            // already using image constraints
+            this.recordImage.deviceId = {exact: deviceId};
         }
 
         // release existing device
